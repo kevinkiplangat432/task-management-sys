955a89fc351dda7c45de8ac2c34e1ca06c488eb0
kevin kiplangat <kiplangatkevin335@gmail.com>
2026-03-30 09:00:55 +0300

    initial commit

--- task_manager/validation.py
+++ task_manager/validation.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
 def validate_task_title(title):
-    if len(title.strip()) == 0:
+    if len(title) == 0:
         raise ValueError("Task title cannot be empty.")
     return title
 
 def validate_task_description(description):
-    if len(description.strip()) == 0:
+    if len(description) == 0:
         raise ValueError("Task description cannot be empty.")
     return description
 
